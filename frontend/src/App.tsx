6e1fabf22ba3951d40092c19228a5b245e089c53
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2024-09-07 20:23:25 -0300

    tailwind ajustado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-//import { useState } from 'react'
+function App() {  
+  return (  
+    <div className="bg-gray-900 h-screen flex items-center justify-center">  
+      <h1 className="text-3xl font-bold text-indigo-800">Olá Mundo</h1>  
+    </div>  
+  );  
+}  
 
-function App() {
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline ">Olá Mundo</h1>
-    </>
-  )
-}
-
-export default App
+export default App;
