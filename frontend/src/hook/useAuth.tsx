753bf83f889b8cd2311be37690c3b35782b231a8
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2024-09-09 21:03:18 -0300

    12

--- frontend/src/hook/useAuth.tsx
+++ frontend/src/hook/useAuth.tsx
@@ -3,4 +3,5 @@ import { AuthContextProps } from "../models/interface";
 
 export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => useContext(AuthContext)
