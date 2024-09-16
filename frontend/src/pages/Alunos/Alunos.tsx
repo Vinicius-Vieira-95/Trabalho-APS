ed25a0a7580cc2e2814c68ce22bd985780ca5fdc
Emerson <emersonlucenaoficial@gmail.com>
2024-09-16 20:50:39 -0300

    fix: move files to proper folder

--- frontend/src/pages/Alunos/Alunos.tsx
+++ frontend/src/pages/Alunos/Alunos.tsx
@@ -1,20 +1,19 @@
-import DatatableAluno from "./components/DatatableAluno";  
-import Sidebar from "./components/Sidebar";  
+import DatatableAluno from "../../components/DatatableAluno";
+import Sidebar from "./components/Sidebar";
 
-const Alunos = () => {  
-    return (  
-        <div className="flex">  
-            <Sidebar/>  
-            <main className="flex-1 bg-slate-50">   
-                <div className="flex items-center justify-center h-screen">  
-                   
-                    <div className="w-[80rem]">  
-                        <DatatableAluno/>  
-                    </div>  
-                </div>  
-            </main>  
-        </div>  
-    );  
-}  
+const Alunos = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 bg-slate-50">
+        <div className="flex items-start p-10 justify-center h-screen">
+          <div className="w-full">
+            <DatatableAluno />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default Alunos;
