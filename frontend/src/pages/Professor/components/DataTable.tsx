c76003e1c66415a5bf269c7582492945233baa33
Vinicius Vieira Carneiro <vinicius.carneiro@aluno.uece.br>
2024-09-18 17:53:12 -0300

    mocando dados

--- frontend/src/pages/Professor/components/DataTable.tsx
+++ frontend/src/pages/Professor/components/DataTable.tsx
@@ -1,11 +1,30 @@
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
-//import TableBody from '@mui/material/TableBody';
+import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+
+const mockTabelaEventosAluno = [
+    {
+      evento: "Prova de Matemática",
+      descricao: "Prova sobre álgebra e geometria.",
+      categoria: "Avaliação"
+    },
+    {
+      evento: "Seminário de Ciências",
+      descricao: "Apresentação sobre biologia molecular.",
+      categoria: "Seminário"
+    },
+    {
+      evento: "Feira de Tecnologia",
+      descricao: "Exposição de projetos de tecnologia.",
+      categoria: "Feira"
+    },
+  ];
+  
 
 
 function DataTable() {
@@ -20,7 +39,7 @@ function DataTable() {
                             <TableCell>Categoria</TableCell>
                         </TableRow>
                     </TableHead>
-{/*                     <TableBody>
+                    <TableBody>
                         {mockTabelaEventosAluno.map((evento, index) => (
                             <TableRow key={index}>
                                 <TableCell>{evento.evento}</TableCell>
@@ -28,7 +47,7 @@ function DataTable() {
                                 <TableCell>{evento.categoria}</TableCell>
                             </TableRow>
                         ))}
-                    </TableBody> */}
+                    </TableBody>
                 </Table>
             </TableContainer>
         </Box>
