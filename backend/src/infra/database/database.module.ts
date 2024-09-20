34d612757309093788532a447560c36c3c55894c
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2024-09-20 19:49:52 -0300

    ajustes

--- backend/src/infra/database/database.module.ts
+++ backend/src/infra/database/database.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { PrismaUserRepository } from '@/infra/database/prisma/repositories/user.repository';
