1fe2520253471a2450e593b9f6e258f31bbab03e
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2024-09-23 22:05:23 -0300

    ajust-dashboard

--- frontend/src/pages/Alunos/Dashboard.tsx
+++ frontend/src/pages/Alunos/Dashboard.tsx
@@ -1,16 +1,19 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import CardDashBoard from "./components/CardDashBoard";
 
-
 const Dashboard = () => {
-    return (
-        <Box className="flex">
-          <Sidebar />
-          <main className="flex-1 bg-slate-50">
-           <CardDashBoard/>
-          </main>
-        </Box>
-      );
-}
+  return (
+    <Box className="flex">
+      <Sidebar />
+      <main className="flex-1 bg-slate-50 ml-64">
+        <div className="flex p-10 justify-center min-h-screen">
+          <div className="w-full overflow-auto">
+            <CardDashBoard />
+          </div>
+        </div>
+      </main>
+    </Box>
+  );
+};
 export default Dashboard;
