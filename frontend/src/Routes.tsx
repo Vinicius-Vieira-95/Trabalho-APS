81a8c66348ced5816b2dfa9320a3243684bb124b
Lucas Ivisson <lucasivisson1@gmail.com>
2024-09-23 22:02:21 -0300

    feat: working with event page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,21 +1,21 @@
-import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';  
-import LoginPage from './pages/Login/LoginPage';  
-import AlunosPage from './pages/Alunos/Alunos';
-import ProfessorPage from './pages/Professor/Professor';  
-import { useAuth } from './hook/useAuth';  
-import Dashboard from './pages/Alunos/Dashboard';
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
+import AlunosPage from "./pages/Alunos/Alunos";
+import ProfessorPage from "./pages/Professor/Professor";
+import { useAuth } from "./hook/useAuth";
+import Dashboard from "./pages/Alunos/Dashboard";
 
-const AppRouter: React.FC = () => {  
-  const auth = useAuth();   
+const AppRouter: React.FC = () => {
+  const auth = useAuth();
 
-  return (  
+  return (
     <BrowserRouter>
       <Routes>
         {/* Rotas públicas */}
         {!auth?.isAuthenticated() && (
           <>
-            <Route path='/' element={<LoginPage />} />
-            <Route path='*' element={<Navigate to='/' />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </>
         )}
 
@@ -24,22 +24,23 @@ const AppRouter: React.FC = () => {
           <>
             {auth.user?.type === 0 && (
               <>
-                <Route path='/alunos' element={<AlunosPage />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='*' element={<Navigate to='/alunos' />} />
+                <Route path="/alunos" element={<AlunosPage />} />
+                <Route path="/dasdas" element={<AlunosPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/alunos" />} />
               </>
             )}
             {auth.user?.type === 1 && (
               <>
-                <Route path='/professor' element={<ProfessorPage />} />
-                <Route path='*' element={<Navigate to='/professor' />} />
+                <Route path="/professor" element={<ProfessorPage />} />
+                <Route path="*" element={<Navigate to="/professor" />} />
               </>
             )}
           </>
         )}
       </Routes>
     </BrowserRouter>
-  );  
-};  
+  );
+};
 
 export default AppRouter;
