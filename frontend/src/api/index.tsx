ac1e1374302ab6f791ab9edabccfba15e70c337e
Emanoel de Moura Silva <62682836+Emanoel-de-Moura-Silva@users.noreply.github.com>
2024-11-07 07:52:28 -0300

    Update index.tsx

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-// Verifique e acesse a variável de ambiente
-const apiBaseUrl =
-  typeof window._env_ === "object" && window._env_ !== null
-    ? (window._env_ as { [key: string]: string }).REACT_APP_API_BASE ||
-      "http://localhost:3000/"
-    : "http://localhost:3000/";
+const apiBaseUrl = (): string => {
+  if (typeof window._env_ === "object" && window._env_ !== null) {
+    const apiBase = window._env_.REACT_APP_API_BASE;
+    if (apiBase && typeof apiBase === "string") {
+      return apiBase;
+    }
+    console.warn("REACT_APP_API_BASE não encontrada ou inválida.");
+  }
+ 
+  return "http://localhost:3000/";
+};
 
-// Configuração do Axios
+
 const api = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: apiBaseUrl(),
   headers: {
     "Content-Type": "application/json",
   },
